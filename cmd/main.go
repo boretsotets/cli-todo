6258ab4009949ee5ba8e19a949060190db9447b7
boretsotets <cachow@bk.ru>
2025-08-31 15:26:17 +0300

    basic functional done

--- cmd/main.go
+++ cmd/main.go
@@ -11,12 +11,11 @@ func main() {
 	case "add": tasks.AddMethod(cli_args)
 	case "update": tasks.UpdateMethod(cli_args[1:])
 	case "delete": tasks.DeleteMethod(cli_args)
-	case "mark-done":
-	case "mark-in-progress":
-	case "list":
+	case "mark-done": tasks.MarkDone(cli_args)
+	case "mark-in-progress": tasks.MarkInProgress(cli_args)
+	case "list": tasks.ListMethod(cli_args)
 	default: tasks.NoSuchMethod(cli_args[0])
 	}
 
 	return
 }
-
--- internal/tasks/methods.go
+++ internal/tasks/methods.go
@@ -15,8 +15,8 @@ type Task struct {
 	Id int
 	Description string
 	Status string
-	CreatedAt time.Time
-	UpdatedAt time.Time
+	CreatedAt string
+	UpdatedAt string
 }
 
 func NoSuchMethod(a string) {
@@ -41,7 +41,9 @@ func AddMethod(a []string) {
 		return
 	}
 
-	task := Task{len(all_data.Tasks)+1, a[1], "todo", time.Now(), time.Now()}
+	formattedTime := time.Now().Format("2006-01-02 15:04:05")
+
+	task := Task{len(all_data.Tasks)+1, a[1], "todo", formattedTime, formattedTime}
 	all_data.Tasks = append(all_data.Tasks, task)
 
 	WriteToFile("./internal/database/tasks_data.json", all_data)
@@ -88,6 +90,7 @@ func UpdateMethod(a []string) {
 	for i := range all_data.Tasks {
 		if all_data.Tasks[i].Id == index {
 			all_data.Tasks[i].Description = a[1]
+			all_data.Tasks[i].UpdatedAt = time.Now().Format("2006-01-02 15:04:05")
 			break
 		} else if i == len(all_data.Tasks)-1 {
 			fmt.Println("There is no task with this id!")
@@ -97,7 +100,86 @@ func UpdateMethod(a []string) {
 	WriteToFile("./internal/database/tasks_data.json", all_data)
 }
 
-func ListMethod() {
-	
+const LISTALL int = 0
+const LISTDONE int = 1
+const LISTTODO int = 2
+const LISTINPROGRESS int = 3
+
+func ListMethod(a []string) {
+	var flag int
+
+	if len(a) > 2 {
+		fmt.Println("write correct option for list method!")
+		return
+	} else if len(a) == 1 {
+		flag = LISTALL
+	} else {
+		switch a[1] {
+			case "done": flag = LISTDONE
+			case "todo": flag = LISTTODO
+			case "in-progress": flag = LISTINPROGRESS
+			default: {
+				NoSuchMethod(a[1])
+				return
+			    }
+		}
+	}
+	PrintList(flag)
 }
 
+func PrintList(flag int) {
+
+	all_data, _ := LoadFromFile()
+
+	for i := range all_data.Tasks {
+		if flag == LISTALL || (flag == LISTDONE && all_data.Tasks[i].Status == "done") ||
+		(flag == LISTTODO && all_data.Tasks[i].Status == "todo") ||
+		(flag == LISTINPROGRESS && all_data.Tasks[i].Status == "in-progress") {
+			fmt.Println(all_data.Tasks[i].Id, all_data.Tasks[i].Description, 
+				all_data.Tasks[i].Status, all_data.Tasks[i].CreatedAt,
+			all_data.Tasks[i].UpdatedAt)
+		}
+	}
+}
+
+func MarkInProgress(a []string) {
+	all_data, _ := LoadFromFile()
+
+	if len(a) != 2 {
+		fmt.Println("needed 2 parameters")
+	} else if neededId, err := strconv.Atoi(a[1]); err != nil {
+		fmt.Println("error parsing id")
+	} else {
+		for i := range all_data.Tasks {
+			if all_data.Tasks[i].Id == neededId {
+				all_data.Tasks[i].Status = "in-progress"
+				all_data.Tasks[i].UpdatedAt = time.Now().Format("2006-01-02 15:04:05")
+				break
+			} else if i == len(all_data.Tasks)-1 {
+				fmt.Println("There is no task with this id!")
+			}
+		}
+	}
+	WriteToFile("./internal/database/tasks_data.json", all_data)
+}
+
+func MarkDone(a []string) {
+	all_data, _ := LoadFromFile()
+	if len(a) != 2 {
+		fmt.Println("needed 2 parameters")
+	} else if neededId, err := strconv.Atoi(a[1]); err != nil {
+		fmt.Println("error parsing id")
+	} else {
+		for i := range all_data.Tasks {
+			if all_data.Tasks[i].Id == neededId {
+				all_data.Tasks[i].Status = "done"
+				all_data.Tasks[i].UpdatedAt = time.Now().Format("2006-01-02 15:04:05")
+				break
+			} else if i == len(all_data.Tasks)-1 {
+				fmt.Println("There is no task with this id!")
+			}
+		}
+	}
+	WriteToFile("./internal/database/tasks_data.json", all_data)
+
+}
